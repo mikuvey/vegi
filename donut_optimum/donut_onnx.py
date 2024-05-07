1c02ea99226e49ba1d52e1c8e8a98a802ed16d52
mikuvey <30137716+mikuvey@users.noreply.github.com>
2024-05-06 18:15:11 -0700

    commit donut_onnx

---
+++ donut_optimum/donut_onnx.py
@@ -0,0 +1,45 @@
+import re
+
+from transformers import DonutProcessor, AutoTokenizer
+from optimum.onnxruntime import ORTModelForVision2Seq
+from datasets import load_dataset
+from PIL import Image
+
+# Load processor, tokenizer, and model
+processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+tokenizer = AutoTokenizer.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+model = ORTModelForVision2Seq.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2", export=True)
+
+#save as .onnx
+save_directory = "tmp/onnx/"
+model.save_pretrained(save_directory)
+tokenizer.save_pretrained(save_directory)
+
+# Load document image
+dataset = load_dataset("hf-internal-testing/example-documents", split="test")
+image = Image.fromarray(dataset[2]["image"])
+
+# Prepare decoder inputs
+task_prompt = "<s_cord-v2>"
+decoder_input_ids = tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+
+# Preprocess the image
+pixel_values = processor(image, return_tensors="pt").pixel_values
+
+# Generate output using ORTModel (Assuming use_cache and other parameters are supported by ORTModelForVision2Seq)
+outputs = model.generate(
+    pixel_values=pixel_values,
+    decoder_input_ids=decoder_input_ids,
+    max_length=model.decoder.config.max_position_embeddings,
+    pad_token_id=tokenizer.pad_token_id,
+    eos_token_id=tokenizer.eos_token_id,
+    use_cache=True,
+    bad_words_ids=[[tokenizer.unk_token_id]],
+    return_dict_in_generate=True,
+)
+
+# Decode the output
+sequence = tokenizer.batch_decode(outputs.sequences)[0]
+sequence = sequence.replace(tokenizer.eos_token, "").replace(tokenizer.pad_token, "")
+sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
+print(processor.token2json(sequence))
